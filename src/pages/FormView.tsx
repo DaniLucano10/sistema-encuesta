2ca5dc3ef218a37094ac2a81bf6688c4771a579e
DaniLucano10 <olucano10@gmail.com>
2024-11-22 14:25:17 -0500

    Agregando pregunta de satisfacción al formulario

--- src/pages/FormView.tsx
+++ src/pages/FormView.tsx
@@ -36,17 +36,20 @@ interface Item {
   };
 }
 
-type Status = 'Enviado' | 'Completado' | 'Pendiente';
-
+type Status = "Enviado" | "Completado" | "Pendiente";
 
 const FormView: React.FC = () => {
-  const [answers, setAnswers] = useState<{ [key: string]: string | string[] }>({});
+  const [answers, setAnswers] = useState<{ [key: string]: string | string[] }>(
+    {}
+  );
   const [selectedQuestion, setSelectedQuestion] = useState<string | null>(null);
   const [formSendedId, setFormSendedId] = useState<string | null>(null);
   const location = useLocation();
   const navigate = useNavigate();
 
-  useState<boolean>(() => new URLSearchParams(location.search).get('submitted') === 'true');
+  useState<boolean>(
+    () => new URLSearchParams(location.search).get("submitted") === "true"
+  );
   const [status, setStatus] = useState<Status>();
 
   useEffect(() => {
@@ -57,9 +60,15 @@ const FormView: React.FC = () => {
     }
   }, [location.search]);
 
-  const formParams = useMemo(() => (formSendedId ? { id: formSendedId } : {}), [formSendedId]);
+  const formParams = useMemo(
+    () => (formSendedId ? { id: formSendedId } : {}),
+    [formSendedId]
+  );
   const { data, loading, error } = useFetchAnswers(formParams);
-  const { register, loading: loadingRegister } = useRegisterAnswer({ close: () => { }, fetch: () => { } });
+  const { register, loading: loadingRegister } = useRegisterAnswer({
+    close: () => { },
+    fetch: () => { },
+  });
 
   useEffect(() => {
     if (formSendedId) {
@@ -75,20 +84,32 @@ const FormView: React.FC = () => {
 
   useEffect(() => {
     if (Array.isArray(data) && data.length > 0 && data[0].form_id?.questions) {
-      const initialAnswers = data[0].form_id.questions.reduce((acc: { [key: string]: string | string[] }, question: Question & { answer?: { text?: string; option_id?: number } }) => {
-        const existingAnswer = question.answer?.text || question.answer?.option_id?.toString();
-        if (existingAnswer) acc[question.id] = existingAnswer;
-        return acc;
-      }, {});
+      const initialAnswers = data[0].form_id.questions.reduce(
+        (
+          acc: { [key: string]: string | string[] },
+          question: Question & {
+            answer?: { text?: string; option_id?: number };
+          }
+        ) => {
+          const existingAnswer =
+            question.answer?.text || question.answer?.option_id?.toString();
+          if (existingAnswer) acc[question.id] = existingAnswer;
+          return acc;
+        },
+        {}
+      );
       setAnswers(initialAnswers);
     }
   }, [data]);
 
-  const handleTextChange = (questionId: string, value: string) => setAnswers(prev => ({ ...prev, [questionId]: value }));
+  const handleTextChange = (questionId: string, value: string) =>
+    setAnswers((prev) => ({ ...prev, [questionId]: value }));
 
   const handleCheckboxChange = (questionId: string, optionId: string) => {
-    setAnswers(prev => {
-      const currentAnswers = Array.isArray(prev[questionId]) ? prev[questionId] : [];
+    setAnswers((prev) => {
+      const currentAnswers = Array.isArray(prev[questionId])
+        ? prev[questionId]
+        : [];
       return {
         ...prev,
         [questionId]: currentAnswers.includes(optionId)
@@ -97,7 +118,6 @@ const FormView: React.FC = () => {
       };
     });
   };
-
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -114,16 +134,29 @@ const FormView: React.FC = () => {
     };
 
     await register(formData);
-    setStatus('Completado');
+    setStatus("Completado");
     localStorage.setItem("formStatus", "Completado");
-    navigate(`${location.pathname}?${new URLSearchParams({ ...Object.fromEntries(new URLSearchParams(location.search)), submitted: 'true' })}`);
-
+    navigate(
+      `${location.pathname}?${new URLSearchParams({
+        ...Object.fromEntries(new URLSearchParams(location.search)),
+        submitted: "true",
+      })}`
+    );
   };
 
   if (loading) return <div>Cargando...</div>;
   if (error) return <div>Error al cargar los datos: {error}</div>;
 
-  if (!data || data.length === 0 || typeof data[0] !== "object" || !("form_id" in data[0]) || typeof (data[0] as { form_id: any }).form_id !== "object" || !Array.isArray((data[0] as { form_id: { questions: any[] } }).form_id.questions)) {
+  if (
+    !data ||
+    data.length === 0 ||
+    typeof data[0] !== "object" ||
+    !("form_id" in data[0]) ||
+    typeof (data[0] as { form_id: any }).form_id !== "object" ||
+    !Array.isArray(
+      (data[0] as { form_id: { questions: any[] } }).form_id.questions
+    )
+  ) {
     return <div>No hay datos disponibles.</div>;
   }
 
@@ -136,146 +169,231 @@ const FormView: React.FC = () => {
     return Array.isArray(value) ? value : value ? [value] : [];
   };
 
-  const allQuestionsAnswered = data?.[0]?.form_id?.questions.every((question: Question) => {
-    const answer = answers[question.id];
-  
-    if (question.type === "Normal") {
-      // Asegura que no sea undefined, vacío o solo espacios
-      return typeof answer === "string" && answer.trim() !== "";
+  const allQuestionsAnswered = data?.[0]?.form_id?.questions.every(
+    (question: Question) => {
+      const answer = answers[question.id];
+
+      if (question.type === "Normal") {
+        // Asegura que no sea undefined, vacío o solo espacios
+        return typeof answer === "string" && answer.trim() !== "";
+      }
+
+      if (question.type === "Multiple") {
+        // Asegura que la respuesta sea un arreglo y tenga al menos un valor
+        return Array.isArray(answer) && answer.length > 0;
+      }
+
+      return true;
     }
-  
-    if (question.type === "Multiple") {
-      // Asegura que la respuesta sea un arreglo y tenga al menos un valor
-      return Array.isArray(answer) && answer.length > 0;
-    }
-  
-    return true; 
-  });
-  
+  );
+
   // Mostrar mensaje de éxito si el estado es "Completado"
-  if (status === 'Completado') {
+  if (status === "Completado") {
     return (
-      <Box bg="#F2F2F2" display="flex" alignItems="flex-start" justifyContent="center" width="100%" height="100vh">
-        <Box width="90%" maxW="700px" mt={2} p={4} bg="#FFFFFF" borderRadius="md" borderTop="10px solid #019CFE">
+      <Box
+        bg="#F2F2F2"
+        display="flex"
+        alignItems="flex-start"
+        justifyContent="center"
+        width="100%"
+        height="100vh"
+      >
+        <Box
+          width="90%"
+          maxW="700px"
+          mt={2}
+          p={4}
+          bg="#FFFFFF"
+          borderRadius="md"
+          borderTop="10px solid #019CFE"
+        >
           {data.map((item: Item) => (
-            <Text key={item.id} color="#2D3748" fontWeight="bold" fontSize={{ base: "2xl", md: "2xl" }}>
+            <Text
+              key={item.id}
+              color="#2D3748"
+              fontWeight="bold"
+              fontSize={{ base: "2xl", md: "2xl" }}
+            >
               {item.form_id?.title}
             </Text>
           ))}
-          <Text color="#718096" fontWeight="bold" fontSize={{ base: "1xl", md: "1xl" }}>
+          <Text
+            color="#718096"
+            fontWeight="bold"
+            fontSize={{ base: "1xl", md: "1xl" }}
+          >
             Su respuesta ha sido registrada correctamente.
           </Text>
-          <Text mt={2} color="#019CFE" cursor="pointer">Gracias</Text>
+          <Text mt={2} color="#019CFE" cursor="pointer">
+            Gracias
+          </Text>
         </Box>
       </Box>
     );
   }
 
-
   return (
     <>
-      {status === 'Enviado' &&
-        (
-          <>
-            <Box>
-              <Box bg="#F2F2F2">
-                <Box display="flex" alignItems="center" justifyContent="center" width="100%">
-                  <Box width="90%" maxW="700px" mt={2}>
-                    {data.map((item: Item) => (
-                      <Box bg="#019CFE" p={{ base: 8, md: 12 }} borderRadius="md" mb={2} key={item.id}>
-                        <Text color="white" fontWeight="bold" textAlign="center" fontSize={{ base: "2xl", md: "3xl" }}>
-                          <Text>{item.form_id.title}</Text>
+      {status === "Enviado" && (
+        <>
+          <Box>
+            <Box bg="#F2F2F2">
+              <Box
+                display="flex"
+                alignItems="center"
+                justifyContent="center"
+                width="100%"
+              >
+                <Box width="90%" maxW="700px" mt={2}>
+                  {data.map((item: Item) => (
+                    <Box
+                      bg="#019CFE"
+                      p={{ base: 8, md: 12 }}
+                      borderRadius="md"
+                      mb={2}
+                      key={item.id}
+                    >
+                      <Text
+                        color="white"
+                        fontWeight="bold"
+                        textAlign="center"
+                        fontSize={{ base: "2xl", md: "3xl" }}
+                      >
+                        <Text>{item.form_id.title}</Text>
+                      </Text>
+                    </Box>
+                  ))}
+                  {data.map((item: Item) => (
+                    <Box
+                      key={item.id}
+                      mb={4}
+                      p={4}
+                      borderRadius="md"
+                      bg="#FFFFFF"
+                      borderTop="10px solid #019CFE"
+                    >
+                      <Text fontSize="2xl" fontWeight="bold" mb={0}>
+                        Detalles de formulario
+                      </Text>
+                      <Text fontSize="md" color="#2D3748" mb={2}>
+                        {item.form_id.description}
+                      </Text>
+
+                      <Flex alignItems="center">
+                        <Text fontSize="xl" fontWeight="bold" color="#2D3748">
+                          Alumno:
                         </Text>
-                      </Box>
-                    ))}
-                    {data.map((item: Item) => (
+                        <Text fontSize="xl" color="#2D3748" ml={4}>
+                          {item.customer_data.name}
+                        </Text>
+                      </Flex>
+
+                      <Flex alignItems="center">
+                        <Text fontSize="xl" fontWeight="bold" color="#2D3748">
+                          Asesor Comercial:
+                        </Text>
+                        <Text fontSize="xl" color="#2D3748" ml={4}>
+                          {item.advisor_data.name}
+                        </Text>
+                      </Flex>
+                    </Box>
+                  ))}
+                  <Box
+                    mb={4}
+                    p={4}
+                    borderRadius="md"
+                    bg="#FFFFFF"
+                  >
+                    <Text fontSize="xl" fontWeight="bold" mb={4}>
+                      ¿Qué tan satisfecho estás con la atención brindada por el
+                      asesor?
+                    </Text>
+                  </Box>
+
+                  <form onSubmit={handleSubmit}>
+                    {data[0]?.form_id?.questions.map((question: Question) => (
                       <Box
-                        key={item.id}
+                        key={question.id}
                         mb={4}
                         p={4}
                         borderRadius="md"
                         bg="#FFFFFF"
-                        borderTop="10px solid #019CFE"
+                        borderStart={
+                          selectedQuestion === question.id
+                            ? "7px solid #019CFE"
+                            : "none"
+                        }
+                        onClick={() => handleQuestionClick(question.id)}
+                        cursor="pointer"
                       >
-                        <Text fontSize="2xl" fontWeight="bold" mb={0}>
-                          Detalles de formulario
-                        </Text>
-                        <Text fontSize="md" color="#2D3748" mb={2}>
-                          {item.form_id.description}
-                        </Text>
-
-                        <Flex alignItems="center">
-                          <Text fontSize="xl" fontWeight="bold" color="#2D3748">
-                            Alumno:
-                          </Text>
-                          <Text fontSize="xl" color="#2D3748" ml={4}>
-                            {item.customer_data.name}
-                          </Text>
-                        </Flex>
-
-                        <Flex alignItems="center">
-                          <Text fontSize="xl" fontWeight="bold" color="#2D3748">
-                            Asesor Comercial:
-                          </Text>
-                          <Text fontSize="xl" color="#2D3748" ml={4}>
-                            {item.advisor_data.name}
-                          </Text>
-                        </Flex>
+                        <FormControl>
+                          <FormLabel htmlFor={question.id}>
+                            {question.text}
+                          </FormLabel>
+                          {question.type === "Normal" ? (
+                            <Input
+                              id={question.id}
+                              value={answers[question.id] || ""}
+                              onChange={(e) =>
+                                handleTextChange(question.id, e.target.value)
+                              }
+                              placeholder="Respuesta"
+                              border="none"
+                              borderBottom="2px solid"
+                              borderColor="gray.300"
+                            />
+                          ) : (
+                            <CheckboxGroup
+                              value={getCheckboxValue(question.id)}
+                            >
+                              {question.options?.map((option) => (
+                                <Box key={option.id}>
+                                  <Checkbox
+                                    value={option.id}
+                                    isChecked={answers[question.id]?.includes(
+                                      option.id
+                                    )}
+                                    onChange={() =>
+                                      handleCheckboxChange(
+                                        question.id,
+                                        option.id
+                                      )
+                                    }
+                                  >
+                                    {option.text}
+                                  </Checkbox>
+                                </Box>
+                              ))}
+                            </CheckboxGroup>
+                          )}
+                        </FormControl>
                       </Box>
                     ))}
-
-                    <form onSubmit={handleSubmit}>
-                      {data[0]?.form_id?.questions.map((question: Question) => (
-                        <Box key={question.id} mb={4} p={4} borderRadius="md" bg="#FFFFFF" borderStart={selectedQuestion === question.id ? "7px solid #019CFE" : "none"} onClick={() => handleQuestionClick(question.id)} cursor="pointer">
-                          <FormControl>
-                            <FormLabel htmlFor={question.id}>{question.text}</FormLabel>
-                            {question.type === "Normal" ? (
-                              <Input
-                                id={question.id}
-                                value={answers[question.id] || ""}
-                                onChange={(e) => handleTextChange(question.id, e.target.value)}
-                                placeholder="Respuesta"
-                                border="none"
-                                borderBottom="2px solid"
-                                borderColor="gray.300"
-                              />
-                            ) : (
-                              <CheckboxGroup value={getCheckboxValue(question.id)}>
-                                {question.options?.map(option => (
-                                  <Box key={option.id}>
-                                    <Checkbox
-                                      value={option.id}
-                                      isChecked={answers[question.id]?.includes(option.id)}
-                                      onChange={() => handleCheckboxChange(question.id, option.id)}
-                                    >
-                                      {option.text}
-                                    </Checkbox>
-                                  </Box>
-                                ))}
-                              </CheckboxGroup>
-                            )}
-                          </FormControl>
-                        </Box>
-                      ))}
-                      <Button mt={4} bg="#09155F" color="white" type="submit"    _hover={{ bg: "#0A3B7A" }}  isDisabled={!allQuestionsAnswered} isLoading={loadingRegister}>
-                        Enviar
-                      </Button>
-                    </form>
-                    <Box mt={8} textAlign="center">
-                      <Text fontSize="sm" color="gray.600">
-                        © {new Date().getFullYear()} Derechos reservados por ITSYSTEMS
-                      </Text>
-                    </Box>
-                    <Box mt={8} />
+                    <Button
+                      mt={4}
+                      bg="#09155F"
+                      color="white"
+                      type="submit"
+                      _hover={{ bg: "#0A3B7A" }}
+                      isDisabled={!allQuestionsAnswered}
+                      isLoading={loadingRegister}
+                    >
+                      Enviar
+                    </Button>
+                  </form>
+                  <Box mt={8} textAlign="center">
+                    <Text fontSize="sm" color="gray.600">
+                      © {new Date().getFullYear()} Derechos reservados por
+                      ITSYSTEMS
+                    </Text>
                   </Box>
+                  <Box mt={8} />
                 </Box>
               </Box>
-
-
             </Box>
-          </>
-        )
-      }
+          </Box>
+        </>
+      )}
     </>
   );
 };
